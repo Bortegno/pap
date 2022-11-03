2654dac21149ab40241e3887129c3de04e64c172
Chutato1108 <112267923+Chutato1108@users.noreply.github.com>
2022-11-03 00:29:45 -0600

    menu pro

---
+++ Museo/Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject menuDePausa;
+    private bool menuOn;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuOn = !menuOn;
+        }
+        if(menuOn==true)
+        {
+            menuDePausa.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible=true;
+        }
+        else
+        {
+            menuDePausa.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible=false;
+        }
+    }
+    public void Continuar()
+    {
+        menuDePausa.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible=true;
+        menuOn=false;
+    }
+    public void Inicio()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
